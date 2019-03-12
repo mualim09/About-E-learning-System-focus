c9d8e8bd370e6bf01be4275cf0b4b1a61d1495fc
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-13 00:03:07 +0800

    dash-classcode

---
+++ dashboard/classroom-code.php
@@ -0,0 +1,55 @@
+﻿  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                <?php echo $_REQUEST['name']?><BR>
+                                Class code:<?php echo $_REQUEST['code']?>
+
+                              
+                            </h2>
+                            <ul class="header-dropdown m-r--5">
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        <i class="material-icons">more_vert</i>
+                                    </a>
+                                    <ul class="dropdown-menu pull-right">
+                                        <li><a href="javascript:void(0);" class=" waves-effect waves-block">Action</a></li>
+                                        <li><a href="javascript:void(0);" class=" waves-effect waves-block">Another action</a></li>
+                                        <li><a href="javascript:void(0);" class=" waves-effect waves-block">Something else here</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="body">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <!-- Nav tabs -->
+                                    <ul class="nav nav-tabs tab-nav-right" role="tablist">
+                                        <li role="presentation" class="active"><a href="#home_animation_2" data-toggle="tab" aria-expanded="true">Stream</a></li>
+                                        <li role="presentation" class=""><a href="#profile_animation_2" data-toggle="tab" aria-expanded="false">Classwork</a></li>
+                                        <li role="presentation" class=""><a href="#messages_animation_2" data-toggle="tab" aria-expanded="false">People</a></li>
+                                    </ul>
+
+                                    <!-- Tab panes -->
+                                    <div class="tab-content">
+                                        <div role="tabpanel" class="tab-pane animated fadeInRight active" id="home_animation_2">
+                                            <?php 
+                                            include('classroom-stream.php');
+                                            ?>
+                                        </div>
+                                        <div role="tabpanel" class="tab-pane animated fadeInRight" id="profile_animation_2">
+                                            <?php 
+                                            include ('classroom-classwork.php');
+                                            ?>
+                                        </div>
+                                        <div role="tabpanel" class="tab-pane animated fadeInRight" id="messages_animation_2">
+                                            <?php 
+                                            include ('classroom-people.php');
+                                            ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
