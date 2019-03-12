651d462d1315eaadd48119ca114802335dc61a38
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-13 00:07:19 +0800

    index

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-﻿<!DOCTYPE html>
+﻿<?php
+/**
+ * @package    DEVELOPMENT OF AN E-LEARNING SYSTEM FOR INFORMATION MANAGEMENT FOR CAVITE STATE UNIVERSITY
+ *
+ * @copyright  Copyright (C) 2019, All rights reserved.
+ * @license    MIT License version or later; see licensing/LICENSE.txt
+ */
+session_start();
+if(isset($_SESSION['login_user']))
+{      
+     header('location: dashboard');
+}
+?>
+<!DOCTYPE html>
 <html>
 <?php 
 include("inc/main-head.php");
@@ -12,7 +25,7 @@ include("inc/main-head.php");
         </div>
         <div class="card">
             <div class="body">
-                <form id="sign_in" method="POST">
+                <div id="sign_in" >
                     <div class="text-center msg">
                         
                         <img src="assets/images/logo.png" alt="CvSU Logo" style="width: 100px;">
@@ -20,41 +33,116 @@ include("inc/main-head.php");
                         <h3>E-Learning System</h3>
                         <small>Login here using your username and password</small>
                     </div>
-                    <div class="input-group">
-                        <span class="input-group-addon">
-                            <i class="material-icons">person</i>
-                        </span>
-                        <div class="form-line">
-                            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                    <ul class="nav nav-tabs">
+                      <li class="active"><a data-toggle="tab" href="#student">Student</a></li>
+                      <li><a data-toggle="tab" href="#instructor">Instructor</a></li>
+                      <li><a data-toggle="tab" href="#admin">Admin</a></li>
+                    </ul>
+
+                    <div class="tab-content">
+                      <div id="student" class="tab-pane fade in active">
+                        <form action="data-login.php" method="POST"  role="form">
+                            
+                       
+                         <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">person</i>
+                            </span>
+                            <div class="form-line">
+                                <input type="text" class="form-control" name="username" placeholder="Student Number" required autofocus>
+                            </div>
                         </div>
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">lock</i>
+                            </span>
+                            <div class="form-line">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-8 p-t-5">
+                                <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
+                                <label for="rememberme">Remember Me</label>
+                            </div>
+                            <div class="col-xs-4">
+                                <button class="btn btn-block bg-pink waves-effect" type="submit" name="submit_student">SIGN IN</button>
+                            </div>
+                        </div>
+                         </form>
+                      </div>
+
+                      <div id="instructor" class="tab-pane fade">
+                        <form action="data-login.php" method="POST"  role="form">
+                            
+                        
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">person</i>
+                            </span>
+                            <div class="form-line">
+                                <input type="text" class="form-control" name="username" placeholder="Instructor ID" required autofocus>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">lock</i>
+                            </span>
+                            <div class="form-line">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-8 p-t-5">
+                                <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
+                                <label for="rememberme">Remember Me</label>
+                            </div>
+                            <div class="col-xs-4">
+                                <button class="btn btn-block bg-pink waves-effect" type="submit" name="submit_instructor">SIGN IN</button>
+                            </div>
+                        </div>
+                        </form>
+                      </div>
+                     <!--  //admin -->
+                      <div id="admin" class="tab-pane fade">
+                        <form action="data-login.php" method="POST"  role="form">
+                         <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">person</i>
+                            </span>
+                            <div class="form-line">
+                                <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">lock</i>
+                            </span>
+                            <div class="form-line">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-8 p-t-5">
+                                <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
+                                <label for="rememberme">Remember Me</label>
+                            </div>
+                            <div class="col-xs-4">
+                                <button class="btn btn-block bg-pink waves-effect" type="submit" name="submit_admin">SIGN IN</button>
+                            </div>
+                        </div>
+                        </form>
+                      </div>
+                     <!--  //admin -->
                     </div>
-                    <div class="input-group">
-                        <span class="input-group-addon">
-                            <i class="material-icons">lock</i>
-                        </span>
-                        <div class="form-line">
-                            <input type="password" class="form-control" name="password" placeholder="Password" required>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-8 p-t-5">
-                            <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
-                            <label for="rememberme">Remember Me</label>
-                        </div>
-                        <div class="col-xs-4">
-                            <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
-                        </div>
-                    </div>
+                   
                     <hr>
                     <div class="row m-t-15 m-b--20">
-                        <div class="col-xs-6">
-                            <a href="sign-up.html">Register Now!</a>
-                        </div>
                         <div class="col-xs-6 align-right">
                             <a href="forgot-password.html">Forgot Password?</a>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
