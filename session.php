8b71fa8461d7351165ef8494c756cc280fa9ebd4
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-13 00:10:06 +0800

    session

--- session.php
+++ session.php
@@ -1,16 +1,23 @@
 <?php
-require_once('dbconfig.php');
-session_start();// Starting Session
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+include("dbconfig.php");
+session_start(); // Starting Session
 // Storing Session
-$user_check=$_SESSION['login_user'];
+$user_check = $_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$ses_sql=mysqli_query($conn,"SELECT * FROM `user_account` ua
-INNER JOIN user_detail ud ON ua.user_ID =ud.user_ID WHERE ua.username='$user_check'");
-$row = mysqli_fetch_assoc($ses_sql);
-$login_session =$row['username'];
-$img_session =$row['detail_img'];
-if(!isset($login_session)){
-mysqli_close($conn); // Closing Connection
-header('Location: ../index.php'); // Redirecting To Home Page
+	$ses_sql = mysqli_query($conn,"SELECT user_Name,user_ID,level_ID FROM user_accounts WHERE user_Name='$user_check'");
+	$row = mysqli_fetch_assoc($ses_sql);
+	$login_session = $row['user_Name'];
+
+	$_SESSION['login_level'] = $row['level_ID'];
+	$_SESSION['login_id'] = $row['user_ID'];
+	$_SESSION['user_Name'] = $row['user_Name'];
+
+
+if (!isset($login_session))
+{
+  mysqli_close($conn); // Closing Connection
+  header('Location: ../index.php'); // Redirecting To Home Page
 }
+
 ?>
