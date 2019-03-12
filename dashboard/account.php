9260d6463ac3d4e8f7e0a7f0dd3be286d9ed2de2
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-13 00:02:51 +0800

    dash-account
    with add function and delete

---
+++ dashboard/account.php
@@ -0,0 +1,321 @@
+﻿ <?php 
+    include('../session.php');
+    include('dash-global-function.php');
+
+   
+    $pagename = "Account Management";
+    $username = $_SESSION['user_Name'];
+    $user_img = "../assets/images/user.png";
+    $user_email = "mail@gmail.com";
+    $script_for_specific_page = "";
+    if(isset($_SESSION['login_level']) )
+    {      
+        $login_level = $_SESSION['login_level'];
+        if ($login_level != 3) {
+         
+          header('location: error404.php');
+        }
+         
+    }
+
+    if (empty($_REQUEST['page'])) {
+        $page = "";
+    }
+    else{
+        $page = $_REQUEST['page'];
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+ <?php
+    include("dash-head.php");
+    ?>
+<body class="theme-green">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <div class="loader">
+            <div class="preloader">
+                <div class="spinner-layer pl-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <p>Please wait...</p>
+        </div>
+    </div>
+    <!-- #END# Page Loader -->
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    <?php 
+        include('dash-topnav.php');
+    ?>
+    <section>
+        <?php 
+        include("dash-sidenav-left.php");
+        ?>
+
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <?php
+              echo '<pre>';
+            var_dump($_SESSION);
+            echo '</pre>';
+            ?>
+             <!-- Account Table -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Account Management
+                            </h2>
+                        </div>
+                        <div class="body">
+                            <div class="table-responsive">
+                                     <div class="removeMessages"></div>
+
+                <button class="btn bg-green pull pull-right" data-toggle="modal" data-target="#addMember" id="addMemberModalBtn">
+                    <span class="glyphicon glyphicon-plus-sign"></span> Add Member
+                </button>
+                <br /> <br /> <br />
+
+                <table class="table table-bordered table-striped table-hover dataTable" id="manageMemberTable">                    
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Level</th>                                                  
+                            <th>Username</th>
+                            <th>Status</th>                             
+                            <th>Option</th>
+                        </tr>
+                    </thead>
+                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Account Table -->
+        </div>
+    </section>
+
+
+    <!-- jquery plugin -->
+    <script type="text/javascript" src="../assets/jquery/jquery.min.js"></script>
+    
+    <!-- include custom index.js -->
+    <script type="text/javascript" src="../assets/js/index.js"></script>
+    <?php 
+        include("dash-js.php");
+    ?>
+</body>
+    <!-- add modal -->
+    <div class="modal fade" tabindex="-1" role="dialog" id="addMember">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title"><span class="glyphicon glyphicon-plus-sign"></span> Add Account</h4>
+          </div>
+          
+          <form class="form-horizontal" action="php_action/create.php" method="POST" id="createMemberForm">
+
+          <div class="modal-body">
+            <div class="messages"></div>
+              <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="name">Username</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                              <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <br>
+              <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="level">Level</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                               <select class="form-control" name="level" id="level" >
+                                <option value="">~~SELECT~~</option>
+                                <option value="1">Student</option>
+                                <option value="0">Instructor</option>
+                                <option value="2">Admin</option>
+                              </select>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <br>
+               <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="email">Email</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                              <input type="text" class="form-control" id="email" name="email" placeholder="Email">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <br>
+               <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="pass">Password</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                              <input type="text" class="form-control" id="pass" name="pass" placeholder="Password">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <br>
+               <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="con_pass">Retype</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                              <input type="text" class="form-control" id="con_pass" name="con_pass" placeholder="Confirm Your Password">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <br>
+              <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="status">Active</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                               <select class="form-control" name="status" id="status" >
+                                <option value="">~~SELECT~~</option>
+                                <option value="1">Activate</option>
+                                <option value="0">Deactivate</option>
+                                <option value="2">Ban</option>
+                              </select>
+                          </div>
+                      </div>
+                  </div>
+              </div>                    
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+          </form> 
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+    <!-- /add modal -->
+
+    <!-- remove modal -->
+    <div class="modal fade" tabindex="-1" role="dialog" id="removeMemberModal">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title"><span class="glyphicon glyphicon-trash"></span> Remove Member</h4>
+          </div>
+          <div class="modal-body">
+            <p>Do you really want to remove ?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" id="removeBtn">Save changes</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+    <!-- /remove modal -->
+    <!-- edit modal -->
+    <div class="modal fade" tabindex="-1" role="dialog" id="editMemberModal">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title"><span class="glyphicon glyphicon-edit"></span> Edit Account</h4>
+          </div>
+
+        <form class="form-horizontal" action="php_action/update.php" method="POST" id="updateMemberForm">         
+
+          <div class="modal-body">
+
+              <div class="edit-messages"></div>
+              <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="editName">Name</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                              <input type="text" class="form-control" id="editName" name="editName" placeholder="">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <br>
+               <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="editEmail">Email</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                              <input type="text" class="form-control" id="editEmail" name="editEmail" placeholder="">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <br>
+              <div class="row clearfix">
+                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                      <label for="editStatus">Active</label>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                      <div class="form-group">
+                          <div class="form-line">
+                               <select class="form-control" name="editStatus" id="editStatus" >
+                                <option value="">~~SELECT~~</option>
+                                <option value="1">Activate</option>
+                                <option value="0">Deactivate</option>
+                                <option value="2">Ban</option>
+                              </select>
+                          </div>
+                      </div>
+                  </div>
+              </div>     
+          </div>
+          <div class="modal-footer editMemberModal">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+          </form>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+    <!-- /edit modal -->
+
+    </div>
+</div>
+</html>
