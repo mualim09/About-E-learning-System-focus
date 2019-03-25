b533dd372a31ebb85e97c141636f55dddf1e7151
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-26 07:53:00 +0800

    sidenav left

--- dashboard/dash-sidenav-left.php
+++ dashboard/dash-sidenav-left.php
@@ -1,3 +1,105 @@
+<?php 
+
+function side_dashboard(){
+    ?>
+    <li
+      <?php if ($GLOBALS['pagename'] == "Dashboard"): ?>
+          class="active"
+      <?php else: ?>
+          
+      <?php endif ?>
+      
+      >
+          <a href="index">
+              <i class="material-icons">home</i>
+              <span>Dashboard</span>
+          </a>
+      </li>
+    <?php
+}
+function side_accoutMngt(){
+    ?>
+     <li
+      <?php if ($GLOBALS['pagename'] == "Account Management"): ?>
+          class="active"
+      <?php else: ?>
+          
+      <?php endif ?>
+      >
+          <a href="manage-account">
+              <i class="material-icons">account_box</i>
+              <span>Account</span>
+          </a>
+      </li>
+    <?php
+}
+function side_studentMngt(){
+    ?>
+    <li
+     <?php if ($GLOBALS['pagename'] == "Student Management"): ?>
+         class="active"
+     <?php else: ?>
+         
+     <?php endif ?>
+     >
+         <a href="manage-student">
+             <i class="material-icons">account_box</i>
+             <span>Student Management</span>
+         </a>
+     </li>
+    <?php
+}
+function side_instrucMngt(){
+    ?>
+    <li
+     <?php if ($GLOBALS['pagename'] == "Instructor Management"): ?>
+         class="active"
+     <?php else: ?>
+         
+     <?php endif ?>
+     >
+         <a href="manage-instructor">
+             <i class="material-icons">account_box</i>
+             <span>Instructor Management</span>
+         </a>
+     </li>
+    <?php
+}
+function side_class(){
+    ?>
+    <li
+     <?php if ($GLOBALS['pagename'] == "Classroom"): ?>
+         class="active"
+     <?php else: ?>
+         
+     <?php endif ?>
+     
+     >
+         <a href="classroom">
+             <i class="material-icons">class</i>
+             <span>Classroom</span>
+         </a>
+     </li>
+    <?php
+}
+function side_query(){
+      ?>
+    <li
+    <?php if ($GLOBALS['pagename'] == "Query Simulator"): ?>
+        class="active"
+    <?php else: ?>
+        
+    <?php endif ?>
+    >
+         <a href="query-simulator">
+            <i class="material-icons">save</i>
+            <span>Query Simulator</span>
+        </a>
+    </li>
+    <?php
+}   
+?>
+
 <!-- Left Sidebar -->
         <aside id="leftsidebar" class="sidebar">
             <!-- User Info -->
@@ -11,133 +113,40 @@
                     
                 </div>
             </div>
-            <!-- #User Info -->
-    <!--         <div class="panel-group full-body" id="accordion_6" role="tablist" aria-multiselectable="true">
-                        <div class="panel panel-success">
-                            <div class="panel-heading" role="tab" id="headingOne_6">
-                                <h4 class="panel-title">
-                                    <a role="button" data-toggle="collapse" data-parent="#accordion_6" href="#collapseOne_6" aria-expanded="false" aria-controls="collapseOne_6" class="collapsed">
-                                        Account 
-                                        <i class="pull-right material-icons" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
-                                    </a>
-                                </h4>
-                            </div>
-                            <div id="collapseOne_6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne_6" aria-expanded="false" style="height: 0px;">
-                                <div class="panel-body " style="padding: 0px;">
-                                <style type="text/css">
-                                    ul.acc li.list-group-item:hover{
-                                        
-                                        background-color: green;
-                                        color: white;
-                                        text-decoration: none;
 
-                                    }
-                                    ul.acc li.list-group-item a:hover{
-                                        color: white;
-                                        text-decoration: none;
-                                    }
-
-                                </style>
-                                 <ul class="acc list-group " style="color: black; list-style-type: none;">
-
-                                    <li class="list-group-item">
-                                            <a href="profile.php" class="toggled waves-effect waves-block">
-                                                <i class="material-icons">settings</i>
-                                                <span>Profile</span>
-                                            </a>
-                                    </li>
-                                    <li class="list-group-item">
-                                            <a href="../logout.php" class="toggled waves-effect waves-block">
-                                                <i class="material-icons">power_settings_new</i>
-                                                <span>Logout</span>
-                                            </a>
-                                    </li>
-                                 </ul>
-                                 
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
             <!-- Menu -->
             <div class="menu">
                  
                 <ul class="list">
                     <li class="header">MAIN NAVIGATION</li>
-                    <li
-                    <?php if ($pagename == "Dashboard"): ?>
-                        class="active"
-                    <?php else: ?>
-                        
-                    <?php endif ?>
+                    <?php 
+                    if ($login_level == 1) {
+                        side_dashboard();
+                        side_class();
+                        side_query();
+                    }
+                    else if ($login_level == 2) {
+                        side_dashboard();
+                        side_class();
+                        side_query();
+                    }
+                     else if ($login_level == 3) {
+                        side_dashboard();
+                        side_accoutMngt();
+                        side_studentMngt();
+                        side_instrucMngt();
+                        side_class();
+                        side_query();
+                    }
+                    else{
+
+                    }
+                   
+                    ?>
                     
-                    >
-                        <a href="index">
-                            <i class="material-icons">home</i>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    <li
-                    <?php if ($pagename == "Account Management"): ?>
-                        class="active"
-                    <?php else: ?>
-                        
-                    <?php endif ?>
-                    >
-                        <a href="manage-account">
-                            <i class="material-icons">account_box</i>
-                            <span>Account</span>
-                        </a>
-                    </li>
-                     <li
-                    <?php if ($pagename == "Student Management"): ?>
-                        class="active"
-                    <?php else: ?>
-                        
-                    <?php endif ?>
-                    >
-                        <a href="manage-student">
-                            <i class="material-icons">account_box</i>
-                            <span>Student Management</span>
-                        </a>
-                    </li>
-                     <li
-                    <?php if ($pagename == "Instructor Management"): ?>
-                        class="active"
-                    <?php else: ?>
-                        
-                    <?php endif ?>
-                    >
-                        <a href="manage-instructor">
-                            <i class="material-icons">account_box</i>
-                            <span>Instructor Management</span>
-                        </a>
-                    </li>
-                    <li
-                    <?php if ($pagename == "Classroom"): ?>
-                        class="active"
-                    <?php else: ?>
-                        
-                    <?php endif ?>
                     
-                    >
-                        <a href="classroom">
-                            <i class="material-icons">class</i>
-                            <span>Classroom</span>
-                        </a>
-                    </li>
-                      <li
-                    <?php if ($pagename == "Reports"): ?>
-                        class="active"
-                    <?php else: ?>
-                        
-                    <?php endif ?>
-                     >
-                         <a href="query-simulator">
-                            <i class="material-icons">save</i>
-                            <span>Query Simulator</span>
-                        </a>
-                    </li>
-                     <li
+                    
+                   <!--   <li
                     <?php if ($pagename == "Reports"): ?>
                         class="active"
                     <?php else: ?>
@@ -153,7 +162,7 @@
                                 <a href="../assets/" class=" waves-effect waves-block" target="_BLANK">List of student</a>
                             </li>
                         </ul>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
             <!-- #Menu -->
