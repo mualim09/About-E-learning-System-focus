cfd1418d632fad31be0cd13aa6448ce276a3a4d1
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-26 07:53:31 +0800

    profile

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -11,10 +11,10 @@
     if(isset($_SESSION['login_level']) )
     {      
         $login_level = $_SESSION['login_level'];
-        if ($login_level != 3) {
+        // if ($login_level != 3) {
          
-          header('location: error404.php');
-        }
+        //   header('location: error404.php');
+        // }
          
     }
 
@@ -73,8 +73,8 @@
                                 <img src="../assets/images/user.png" alt="AdminBSB - Profile Image" />
                             </div>
                             <div class="content-area">
-                                <h3>Marc K. Hammond</h3>
-                                <p>Web Software Developer</p>
+                                <h3><?php echo $username?></h3>
+                                
                                 <p>Administrator</p>
                             </div>
                         </div>
@@ -93,10 +93,10 @@
                                     <div role="tabpanel" class="tab-pane fade in active" id="profile">
                                         <form class="form-horizontal">
                                             <div class="form-group">
-                                                <label for="NameSurname" class="col-sm-2 control-label">Name Surname</label>
+                                                <label for="NameSurname" class="col-sm-2 control-label">Username</label>
                                                 <div class="col-sm-10">
                                                     <div class="form-line">
-                                                        <input type="text" class="form-control" id="NameSurname" name="NameSurname" placeholder="Name Surname" value="Marc K. Hammond" required>
+                                                        <input type="text" class="form-control" id="NameSurname" name="NameSurname" placeholder="Name Surname" value="<?php echo $username?>" required>
                                                     </div>
                                                 </div>
                                             </div>
@@ -106,31 +106,6 @@
                                                     <div class="form-line">
                                                         <input type="email" class="form-control" id="Email" name="Email" placeholder="Email" value="example@example.com" required>
                                                     </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="InputExperience" class="col-sm-2 control-label">Experience</label>
-
-                                                <div class="col-sm-10">
-                                                    <div class="form-line">
-                                                        <textarea class="form-control" id="InputExperience" name="InputExperience" rows="3" placeholder="Experience"></textarea>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="InputSkills" class="col-sm-2 control-label">Skills</label>
-
-                                                <div class="col-sm-10">
-                                                    <div class="form-line">
-                                                        <input type="text" class="form-control" id="InputSkills" name="InputSkills" placeholder="Skills">
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="form-group">
-                                                <div class="col-sm-offset-2 col-sm-10">
-                                                    <input type="checkbox" id="terms_condition_check" class="chk-col-red filled-in" />
-                                                    <label for="terms_condition_check">I agree to the <a href="#">terms and conditions</a></label>
                                                 </div>
                                             </div>
                                             <div class="form-group">
