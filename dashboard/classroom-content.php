34f7a39378757d5f53075a40fb983f21561a50f3
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-04-01 04:57:19 +0800

    classroom

--- dashboard/classroom-content.php
+++ dashboard/classroom-content.php
@@ -1,15 +1,30 @@
-﻿            
+﻿   <ol class="breadcrumb breadcrumb-bg-green">
+                <li><a href="index"><i class="material-icons">home</i> Dashboard</a></li>
+                <li  class="active"><a href="javascript:void(0);"><i class="material-icons ">account_box</i> Classrooms</a></li>
+            </ol>            
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <div class="header">
                             <h2>
-                                Enrolled Classrrom
-                                <small>With a bit of extra markup, it's possible to add any kind of HTML content like headings, paragraphs, or buttons into thumbnails.</small>
+                                Enrolled Classroom
+                                <small></small>
                             </h2>
                             <div class="pull-right">
                                 <div class="btn-group" role="group">
-                                    <button type="button" class="btn bg-green waves-effect" data-toggle="modal" data-target="#JoinClass"><i class="material-icons">person_add</i> Join Class</button>
-                                    <button type="button" class="btn bg-green waves-effect" data-toggle="modal" data-target="#CreateClass">    <i class="material-icons">add_circle</i> Create Class</button>
+                                    <?php 
+                                    if ($login_level == 1) {
+                                        ?>
+                                         <button type="button" class="btn bg-green waves-effect" data-toggle="modal" data-target="#JoinClass"><i class="material-icons">person_add</i> Join Class</button>
+                                        <?php
+                                    }
+                                    else{
+                                        ?>
+                                        <button type="button" class="btn bg-green waves-effect" data-toggle="modal" data-target="#CreateClass">    <i class="material-icons">add_circle</i> Create Class</button>
+                                        <?php
+                                    }
+                                    ?>
+                                   
+                                    
                                 </div>
                             </div>
                             <br>
@@ -19,8 +34,17 @@
             <div class="row clearfix">
                 <?php 
                 
-                 $query = mysqli_query($conn,"SELECT * FROM `class_room`");
-               
+                
+                if ($login_level == 1) {
+                    $sql = "SELECT * FROM `class_student` `cls`
+                    INNER JOIN `class_room` `clr`  ON `cls`.`class_ID` = `clr`.`class_ID`  WHERE `cls`.user_ID =  $user_id AND join_Stat = 1";
+                    $query = mysqli_query($conn, $sql);
+                }
+                else
+                {
+                    $sql  = "SELECT * FROM `class_room` WHERE user_ID = $user_id";
+                    $query = mysqli_query($conn,$sql);
+                }
                  
                 if (mysqli_num_rows($query) > 0) {
                     // output data of each row
@@ -29,13 +53,17 @@
                         $class_ID = $classroom['class_ID'];
                         $class_Code = $classroom['class_Code'];
                         $class_Name = $classroom['class_Name'];
+                        $class_Description = $classroom['class_Description'];
                         $class_Color = $classroom['class_Color'];
+                        $class_status = $classroom['class_status'];
+                        $sql = "SELECT * FROM `class_student` WHERE class_ID = $class_ID ";
+                        $count_s = mysqli_query($conn,$sql);
                         ?>
                         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                         <div class="card">
                             <div class="header <?php  bg_color($class_Color)?>">
                                 <h2>
-                                    <a href="classroom?name=<?php echo $class_Name?>&code=<?php echo $class_Code?>&classID=<?php echo $class_ID?>" style="color: white;"><?php echo $class_Name;?></a> <small>0 Student</small>
+                                    <a href="classroom?name=<?php echo $class_Name?>&code=<?php echo $class_Code?>&classID=<?php echo $class_ID?>" style="color: white;"><?php echo $class_Name;?></a> <small><?php echo mysqli_num_rows($count_s);?> Student</small>
                                 </h2>
                                 <ul class="header-dropdown m-r--5">
                                     <li class="dropdown">
@@ -43,27 +71,35 @@
                                             <i class="material-icons">more_vert</i>
                                         </a>
                                         <ul class="dropdown-menu pull-right">
-                                            <li><a href="javascript:void(0);">Action</a></li>
-                                            <li><a href="javascript:void(0);">Another action</a></li>
-                                            <li><a href="javascript:void(0);">Something else here</a></li>
+
+                                            <li><a href="javascript:void(0);">Edit</a></li>
+                                            <?php
+                                            if ($class_status == 1) {
+                                               ?>
+                                               <li><a href="javascript:void(0);"  onclick="disableClassroom(<?php echo $class_ID;?>)">Disabled</a></li>
+                                                <?php
+                                            }
+                                            else{
+                                                ?>
+                                                 <li><a href="javascript:void(0);"  onclick="enableClassroom(<?php echo $class_ID;?>)">Enable</a></li>
+                                                <?php
+                                            }
+                                            ?>
                                         </ul>
                                     </li>
                                 </ul>
                             </div>
-                            <div class="body" style="max-height: 115px;overflow-y: scroll;">
-                                Quis pharetra a pharetra fames blandit. Risus faucibus velit Risus imperdiet mattis neque volutpat, etiam lacinia netus dictum magnis per facilisi sociosqu. Volutpat. Ridiculus nostra.
-                                Quis pharetra a pharetra fames blandit. Risus faucibus velit Risus imperdiet mattis neque volutpat, etiam lacinia netus dictum magnis per facilisi sociosqu. Volutpat. Ridiculus nostra.
-                                Quis pharetra a pharetra fames blandit. Risus faucibus velit Risus imperdiet mattis neque volutpat, etiam lacinia netus dictum magnis per facilisi sociosqu. Volutpat. Ridiculus nostra.
-                                Quis pharetra a pharetra fames blandit. Risus faucibus velit Risus imperdiet mattis neque volutpat, etiam lacinia netus dictum magnis per facilisi sociosqu. Volutpat. Ridiculus nostra.
+                            <div class="body" style="min-height: 115px;max-height: 115px;overflow-y: scroll;">
+                                <?php  echo $class_Description;?>
                             </div>
                         </div>
                     </div>
                         <?php
                     }
                 } else {
-                     echo "<script>alert('Sql Error');
-                                                        window.location='index.php';
-                                                    </script>";
+                   ?>
+
+                   <?php
                 }
                 ?>
                 
@@ -73,3 +109,4 @@
                         </div>
                     </div>
                 </div>
+
