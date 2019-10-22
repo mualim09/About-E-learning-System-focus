92f7843d1a415c069627972687514de7ee45c663
Rhalp Darren Cabrera <rhalpdarrencabrera@gmail.com>
2019-10-22 20:00:37 +0800

    dataTable Classroom Materials

--- dashboard/datatable/classroom_materials/fetch.php
+++ dashboard/datatable/classroom_materials/fetch.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../class.function.php');
 $account = new DTFunction();  		 // Create new connection by passing in your configuration array
-
+session_start();
 
 $query = '';
 $output = array();
@@ -51,17 +51,25 @@ foreach($result as $row)
 	$sub_array[] = $attachment_Name[0];
 	$sub_array[] = $row["attachment_MIME"];
 	$dl = 'href="data:'.$row["attachment_MIME"].';base64,'.base64_encode($row['attachment_Data']).'"';
+	$dlx = 'data:'.$row["attachment_MIME"].';base64,'.base64_encode($row['attachment_Data']).'';
+	// $dl = 'href="view_file?data='.$row["attachment_ID"].'"';
+	  if($account->student_level()){
+    	$btnac = '<a class="btn btn-success btn-sm"  '.$dl.' download="">Download</a>
+    <a class="btn btn-info btn-sm"  href="preview?attachment='.$row["attachment_ID"].'" target="_BLANK">Preview</a>
+
+    ';
+    }
+    else{
+    	$btnac = '<a class="btn btn-success btn-sm "  '.$dl.' download="">Download</a>
+    <a class="btn btn-info btn-sm"  href="preview?attachment='.$row["attachment_ID"].'" target="_BLANK">Preview</a>
+
+    <button type="button" class="btn btn-danger btn-sm delete_material" id="'.$row["attachment_ID"].'">Delete</button>';
+    }
 	$sub_array[] = '
-<div class="btn-group">
-  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Action
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item "  '.$dl.' download="">Download</a>
-     <div class="dropdown-divider"></div>
-    <a class="dropdown-item delete_material" id="'.$row["attachment_ID"].'">Delete</a>
-  </div>
-</div>';
+	<div class="btn-group" role="group" aria-label="Basic example">
+	'.$btnac.'
+	</div>
+	';
 	$data[] = $sub_array;
 }
 
